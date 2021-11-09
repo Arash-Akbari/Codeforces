8598e6864853a4b23198520e930ab726dec54e83
Arash Akbari <arashakbari.mail@gmail.com>
2021-11-09 16:48:46 +0330

    Add files via upload

---
+++ A. Watermelon.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{ int w; 
+
+cin >> w; 
+
+if(w%2==0 && w !=2)
+{ 
+cout <<endl;
+
+cout <<"Yes";
+
+cout <<'\a';
+
+}else{
+	cout << endl;
+	
+	cout <<"No";
+	
+cout <<'\a';
+
+}
+getch ();
+}
